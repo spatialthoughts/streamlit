a53c474bfed1c94f496870de2df3adb8571a1809
Ujaval Gandhi <ujaval@spatialthoughts.com>
2022-08-30 15:58:50 +0530

    'update'

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import folium
 import leafmap.foliumap as leafmap
 import geopandas as gpd
 
+st.set_page_config(page_title="Dashboard", layout="wide")
+
 st.title('National Highway Dashboard')
 
+st.sidebar.title("About")
+st.sidebar.info('Explore the Roads')
+        
 filename = 'karnataka.gpkg'
 
 @st.cache
@@ -12,22 +18,57 @@ def get_data(layer):
     return gdf
 
 districts_gdf = get_data('karnataka_districts')
+roads_gdf = get_data('karnataka_major_roads')
+
+
+@st.cache
+def get_roads_by_district(roads_gdf, districts_gdf):
+    roads_reprojected = roads_gdf.to_crs('EPSG:32643')
+    roads_reprojected['length'] = roads_reprojected['geometry'].length
+    districts_reprojected = districts_gdf.to_crs('EPSG:32643')
+    joined = gpd.sjoin(roads_reprojected, districts_reprojected, how='left', op='intersects')
+    results = joined.groupby('DISTRICT')['length'].sum()/1000
+    return results.to_dict()
+
+nh_gdf = roads_gdf[roads_gdf['ref'].str.match('^NH') == True]
+nh_lengths = get_roads_by_district(nh_gdf, districts_gdf)
 
 districts = districts_gdf.DISTRICT.values
-district = st.selectbox('Select a District', districts)
+district = st.sidebar.selectbox('Select a District', districts)
 
-m = leafmap.Map()
+stats = st.sidebar.text(nh_lengths[district])
 
-districts_gdf.explore(
-    m=m,
-    color='black', 
-    style_kwds={'fillOpacity': 0.3, 'weight': 0.5},
-    tiles='Stamen Terrain',
-    name='districts')
+m = leafmap.Map(
+    layers_control=True,
+    draw_control=False,
+    measure_control=False,
+    fullscreen_control=False,
+)
 
-bounds = districts_gdf.total_bounds
-sw = bounds[1], bounds[0]
-ne = bounds[3], bounds[2]
-m.fit_bounds([sw, ne]) 
+m.add_gdf(
+    gdf=districts_gdf,
+    layer_name='districts',
+    zoom_to_layer=True,
+    info_mode='on_click',
+    style={'color': 'black', 'fillOpacity': 0.3, 'weight': 0.5},
+    )
 
-m.to_streamlit(800, 800)
+m.add_gdf(
+    gdf=nh_gdf,
+    layer_name='national_highways',
+    zoom_to_layer=False,
+    info_mode=None,
+    style={'color': 'blue', 'weight': 0.5},
+    )
+    
+selected_gdf = districts_gdf[districts_gdf['DISTRICT'] == district]
+
+m.add_gdf(
+    gdf=selected_gdf,
+    layer_name='selected',
+    zoom_to_layer=False,
+    info_mode=None,
+    style={'color': 'yellow', 'fill': None, 'weight': 2}
+ )
+
+m_streamlit = m.to_streamlit(800, 800)
